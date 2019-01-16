dfc2f19edba688c9cd346fbf2b1faad52bfa2529
HellBoys <irwin.noteam.2009@gmail.com>
2019-01-16 23:19:52 +0300

    add test application for parsing bufr

---
+++ cmd/example-get-bufr/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/mtfelian/go-eccodes/native"
+
+	"github.com/amsokol/go-errors"
+	codes "github.com/mtfelian/go-eccodes"
+	cio "github.com/mtfelian/go-eccodes/io"
+)
+
+func main() {
+	fmt.Println("Start")
+	f, err := cio.OpenFile("JUVE00 EGRR 161200", "r")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	file, err := codes.OpenFile(f)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	n := 0
+	for {
+		err = process(file, n)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("failed to get message (#%d) from index: %s", n, err.Error())
+		}
+		n++
+	}
+}
+
+func process(file codes.File, n int) error {
+	msg, err := file.Next()
+	if err != nil {
+		return err
+	}
+	defer msg.Close()
+	fmt.Println(msg)
+	if err := msg.SetLong("unpack", 1); err != nil {
+		return errors.Wrap(err, "unpack")
+	}
+
+	if err := printField(msg, "dataCategory", "l"); err != nil {
+		return err
+	}
+	if err := printField(msg, "typicalDate", "l"); err != nil {
+		return err
+	}
+	if err := printField(msg, "stationNumber", "l"); err != nil {
+		return err
+	}
+	if err := printField(msg, "airTemperatureAt2M", "d"); err != nil {
+		return err
+	}
+	arr, err := native.Ccodes_get_long_array(msg.Handle(), "bufrdcExpandedDescriptors")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func printField(msg codes.Message, name, typ string) error {
+	var v interface{}
+	var err error
+	switch typ {
+	case "l":
+		v, err = msg.GetLong(name)
+	case "d":
+		v, err = msg.GetDouble(name)
+	}
+	if err != nil {
+		return errors.Wrapf(err, "field: %s", name)
+	}
+	fmt.Printf("%s: %v\n", name, v)
+	return nil
+}
