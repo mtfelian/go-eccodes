1cb6c5e31e045dcf71f2ba7146f531e0ec546181
HellBoys <irwin.noteam.2009@gmail.com>
2019-01-22 13:11:32 +0300

    set some funcs as public

--- bufr/common.go
+++ bufr/common.go
@@ -2,7 +2,7 @@ package bufr
 
 import codes "github.com/mtfelian/go-eccodes"
 
-func getInt(msg codes.Message, key string) int         { v, _ := msg.GetLong(key); return int(v) }
-func getInt64(msg codes.Message, key string) int64     { v, _ := msg.GetLong(key); return v }
-func getFloat64(msg codes.Message, key string) float64 { v, _ := msg.GetDouble(key); return v }
-func getString(msg codes.Message, key string) string   { v, _ := msg.GetString(key); return v }
+func GetInt(msg codes.Message, key string) int         { v, _ := msg.GetLong(key); return int(v) }
+func GetInt64(msg codes.Message, key string) int64     { v, _ := msg.GetLong(key); return v }
+func GetFloat64(msg codes.Message, key string) float64 { v, _ := msg.GetDouble(key); return v }
+func GetString(msg codes.Message, key string) string   { v, _ := msg.GetString(key); return v }
